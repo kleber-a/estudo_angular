ab55463398cb7cd12af098b5ce60a07cacbe0be7
Kleber-a <kleberandrade1998@hotmail.com>
2022-09-12 22:15:29 -0300

    primeiro commit

---
+++ php/listar.php
@@ -0,0 +1,32 @@
+<?php 
+
+    //Incluir conexão
+    include("conexao.php");
+
+    //Sql
+    $sql = "SELECT * FROM cursos";
+
+    //Executar
+    $executar = mysqli_query($conexao, $sql);
+
+    //Vetor
+    $cursos = [];
+
+    //Índice
+    $indice = 0;
+
+    //Laço
+    while($linha = mysqli_fetch_assoc($executar)){
+        $cursos[$indice]['idCurso'] = $linha['idCurso'];
+        $cursos[$indice]['nomeCurso']= $linha['nomeCurso'];
+        $cursos[$indice]['valorCurso']= $linha['valorCurso'];
+
+        $indice++;
+    }
+
+    json_encode(['cursos'=>$cursos]);
+
+    var_dump($cursos);
+
+
+?>
